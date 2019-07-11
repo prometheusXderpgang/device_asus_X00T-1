85c5e9034c32999ca35fdac10c590abd33d4fc39
prometheusXderpgang <prometheusXderpgang@gmail.com>
2019-07-11 11:17:30 +0530

    X00T: Nuke Dirac
    Syberia don't have Dirac in their repo. So need of keeping it.

--- device.mk
+++ device.mk
@@ -157,10 +157,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
-# Dirac
-PRODUCT_PACKAGES += \
-    Dirac
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
